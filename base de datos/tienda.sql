1b1b32f1d377c604aab18b2c8ba60554956069b1
OliveroReyes <162989776+OliveroReyes@users.noreply.github.com>
2024-05-02 08:33:27 -0300

    Add files via upload

--- base de datos/tienda.sql
+++ base de datos/tienda.sql
@@ -1,1 +1,58 @@
-a
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1:3306
+-- Tiempo de generación: 02-05-2024 a las 11:30:10
+-- Versión del servidor: 8.0.31
+-- Versión de PHP: 8.0.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `tienda`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `productos`
+--
+
+DROP TABLE IF EXISTS `productos`;
+CREATE TABLE IF NOT EXISTS `productos` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(255) NOT NULL,
+  `precio` decimal(10,2) NOT NULL,
+  `cant_des` int NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Volcado de datos para la tabla `productos`
+--
+
+INSERT INTO `productos` (`id`, `nombre`, `precio`, `cant_des`) VALUES
+(1, 'Minecraft', '10.50', 1000000000),
+(2, 'Asphalt 9', '20.75', 800000),
+(3, 'eFootball 2024', '15.00', 786000),
+(4, 'call of duty', '105.00', 687000),
+(5, 'Hollow Knight', '15.00', 680000),
+(6, 'Forza Horizon 4', '25.00', 678000),
+(7, 'Gears 5', '30.00', 670000),
+(8, 'Persona 5 Royal ', '50.00', 668000),
+(9, 'Another Crab\'s', '15.00', 665000),
+(10, 'Roblox', '0.00', 660000);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
